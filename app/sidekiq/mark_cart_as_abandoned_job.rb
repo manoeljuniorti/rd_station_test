f1bec762391e40887e26fa4ed3f353a0b9029312
Manoel Junior <manoeljuniordev@gmail.com>
2025-06-09 16:08:21 -0300

    Added job and new spec

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,14 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
+  queue_as :default
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  def perform
+    now = Time.current
+
+    Cart.inactive_since(3.hours.ago).not_yet_abandoned.find_each do |cart|
+      cart.update!(abandoned_at: now)
+    end
+
+    Cart.abandoned_before(7.days.ago).destroy_all
   end
 end
