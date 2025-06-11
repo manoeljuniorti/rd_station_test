c486575060f952a76fafd47696a26d042851bcf1
Manoel Junior <manoeljuniordev@gmail.com>
2025-06-11 10:06:12 -0300

    Remove unecessary validate

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   scope :active, -> { where(abandoned_at: nil) }
   scope :inactive_since, ->(time) { where("updated_at < ?", time) }
