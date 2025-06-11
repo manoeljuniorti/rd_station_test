8b6959fd63bbb0f0e45d3ea3878dbce21801c44e
Manoel Junior <manoeljuniordev@gmail.com>
2025-06-10 21:31:18 -0300

    Error handling included

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,11 +3,26 @@ module Api
     class BaseController < ApplicationController
       before_action :load_cart
 
+      rescue_from ApplicationService::ItemNotFound, with: :render_item_not_found
+      rescue_from ApplicationService::InvalidQuantity, with: :render_invalid_quantity
+
       private
 
       def load_cart
         @cart = Cart.find_by(id: session[:cart_id]) || Cart.create!
         session[:cart_id] ||= @cart.id
+      end
+
+      def render_item_not_found
+        render_error(:item_not_found, :not_found)
+      end
+
+      def render_invalid_quantity
+        render_error(:invalid_quantity, :unprocessable_entity)
+      end
+
+      def render_error(key, status)
+        render json: { error: I18n.t("errors.#{key}") }, status: status
       end
 
       def cart_payload(cart)
